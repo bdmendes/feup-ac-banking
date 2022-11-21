2f39b9862d5b1976b2b14dc20d54868af4a4f66f
fernandorego <fernandoluisrego@gmail.com>
2022-11-21 23:48:51 +0000

    Merge with data-processing

--- src/preprocess/util.py
+++ src/preprocess/util.py
@@ -17,3 +17,32 @@ def extract_date(df: pd.DataFrame, date_column, extract_gender: bool = False) ->
 def korunas_to_euros(df: pd.DataFrame, column: str) -> pd.DataFrame:
     df[column] = df[column].apply(lambda x: float("{:.2f}".format(x / 24.4)))
     return df
+
+def account_date_to_levels(df: pd.DataFrame):
+    data_range = []
+
+    for i in df.index:
+        loan_data = datetime.datetime(df['year'][i], df['month'][i], df['day'][i])
+        account_data = datetime.datetime(df['account_year'][i], df['account_month'][i], df['account_day'][i])
+
+        if loan_data < account_data:
+            print("loan data is before account data")
+            data_range.append(-1)
+        else:
+            diff_data = loan_data - account_data
+            if diff_data.days < 92:
+                data_range.append(0)
+            elif diff_data.days < 138:
+                data_range.append(1)
+            elif diff_data.days < 183:
+                data_range.append(2)
+            elif diff_data.days < 365:
+                data_range.append(3)
+            elif diff_data.days < 730:
+                data_range.append(4)
+            else:
+                data_range.append(5)
+
+    df['account_level'] = data_range
+    df = df.drop(columns=['account_year', 'account_month', 'account_day'])
+    return df
